33b5b313740d12c15828101b9f989628216f0b40
Sikan He <sikanh@gmail.com>
2017-08-05 16:24:29 -0400

    added package info, 0.1.0-alpha.1

--- mix.exs
+++ mix.exs
@@ -1,14 +1,25 @@
 defmodule ApolloTracing.Mixfile do
   use Mix.Project
 
+  @version "0.1.0-alpha.1"
+
   def project do
     [
       app: :apollo_tracing,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5-rc",
       start_permanent: Mix.env == :prod,
+      package: package(),
       deps: deps()
     ]
+  end
+
+  defp package do
+    [description: "Apollo Tracing middleware for Absinthe",
+     files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Sikanhe"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/sikanhe/apollo-tracing-elixir"}]
   end
 
   def application do
@@ -19,7 +30,8 @@ defmodule ApolloTracing.Mixfile do
 
   defp deps do
     [
-      {:absinthe, "~> 1.4.0-beta.3"}
+      {:absinthe, "~> 1.4.0-beta.3"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
